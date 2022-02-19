b492190844cfcbea03564d99816f3103fb9a1244
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-02-19 04:04:06 +0100

    Show Footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,6 +3,6 @@
   <main class="container mx-auto py-10 text-gray-700 dark:text-gray-200">
     <router-view />
   </main>
-  <!-- <Footer class="container mx-auto" /> -->
+  <Footer class="container mx-auto" />
   <NavigationRail />
 </template>
