4bc39b28d16a783fdffd43d0decce4518d4c48d4
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-03-16 16:30:22 +0100

    Increase NavigationBar height and spacing

--- src/components/global/NavigationBar.vue
+++ src/components/global/NavigationBar.vue
@@ -8,25 +8,25 @@ const items = [
 
 <template>
   <div class="w-full fixed bottom-0 bg-void-600 rounded-t-2xl md:hidden">
-    <nav class="max-w-lg h-16 mx-auto flex justify-around text-gray-200">
+    <nav class="max-w-lg h-18 mx-auto flex justify-around text-gray-200">
       <router-link v-for="item in items" :key="item.url" :to="item.url" class="flex items-center">
         <div>
-          <div class="pill w-12 h-26px flex items-center justify-center text-gray-300">
+          <div class="pill w-50px h-26px flex items-center justify-center text-gray-300 rounded-2xl">
             <span class="iconify w-5 h-auto" :data-icon="item.icon" />
           </div>
-          <div class="text-center text-xs font-medium pt-0.5">
+          <div class="text-center text-xs font-medium pt-1">
             {{ item.label }}
           </div>
         </div>
       </router-link>
     </nav>
   </div>
-  <div class="h-16" />
+  <div class="h-18" />
 </template>
 
 <style scoped>
 .router-link-active .pill {
-  @apply rounded-2xl bg-void-100 !text-void-600;
+  @apply bg-void-100 !text-void-500;
 }
 
 .router-link-active {
