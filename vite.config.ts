f8fa105f38b8410953148adb01ef5fdb3b78a79b
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-03-19 11:02:15 +0100

    Try injectManifest strategy for service worker

--- vite.config.ts
+++ vite.config.ts
@@ -107,6 +107,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt', 'safari-pinned-tab.svg'],
+      strategies: 'injectManifest',
       manifest: {
         name: 'DHBW Mehms',
         short_name: 'DHBW Mehms',
